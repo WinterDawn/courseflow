e3dd75224fdfd74fb3504a8e3f72f3552a1bc010
dowu6943 <dowu6943@colorado.edu>
2018-09-16 23:28:37 -0600

    add login

---
+++ login.js
@@ -0,0 +1,52 @@
+var express=require('express');
+var app=express();
+var mysql=require('mysql');
+ 
+
+var connection = mysql.createConnection({
+    host     : '127.0.0.1',
+    user     : 'root',
+    password : '',
+    database : 'courseflow',
+    port:'3306'
+});
+connection.connect();
+app.get('/',function (req,res) {
+    res.sendfile(__dirname + "/" + "index.html" );
+})
+ 
+
+app.get('/login',function (req,res) {
+    var  name=req.query.name;
+    var pwd=req.query.pwd;
+ 
+    var selectSQL = "select * from user where uname = '"+name+"' and pwd = '"+pwd+"'";
+    connection.query(selectSQL,function (err,rs) {
+        if (err) throw  err;
+        console.log(rs);
+        console.log('OK');
+        res.sendfile(__dirname + "/" + "OK.html" );
+    })
+})
+ 
+// app.get('/register.html',function (req,res) {
+//     res.sendfile(__dirname+"/"+"register.html");
+// })
+ 
+
+// app.get('/register',function (req,res) {
+//     var  name=req.query.name;
+//     var  pwd=req.query.pwd;
+//     var  user={uname:name,pwd:pwd};
+//     connection.query('insert into user set ?',user,function (err,rs) {
+//         if (err) throw  err;
+//         console.log('ok');
+//         res.sendfile(__dirname + "/" + "index.html" );
+//     })
+// })
+ 
+
+var  server=app.listen(7744,function () {
+    console.log("start");
+})
+
